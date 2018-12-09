0a7fc834b060d4d5fa90414096e4a0cc9fc53bb1
ljsdyspg <33971716+ljsdyspg@users.noreply.github.com>
2018-12-09 12:59:52 +0800

    new

--- app/src/main/java/com/test/spg/djirouteapp/WaypointCoord.java
+++ app/src/main/java/com/test/spg/djirouteapp/WaypointCoord.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class WaypointCoord {
     static int size = 100;
     static float altitude = 3.0f;
-    static float speed = 10.0f;
+    static float speed = 1.0f;
     static double[] coord= new double[size];
     static ArrayList coords = new ArrayList();
 
@@ -13,13 +13,9 @@ public class WaypointCoord {
     public static void initWayPoint() {
         double[] temp;
         coords.clear();
-        coords.add(new double[]{30.5309412900,114.3563523600});
-        coords.add(new double[]{30.5309542900,114.3551733600});
-        coords.add(new double[]{30.5291012900,114.3551673600});
-        coords.add(new double[]{30.5290782900,114.3565893600});
-        coords.add(new double[]{30.5290872900,114.3579843600});
-        coords.add(new double[]{30.5309452900,114.3580373600});
-        coords.add(new double[]{30.5309612500,114.3598595400});
-        coords.add(new double[]{30.5292842500,114.3598535400});
+        coords.add(new double[]{30.5303652900,114.3555823600});
+        coords.add(new double[]{30.5303602900,114.3559153600});
+        coords.add(new double[]{30.5301992900,114.3559583600});
+        coords.add(new double[]{30.5302032900,114.3556093600});
     }
 }
